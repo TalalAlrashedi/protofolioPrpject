c0634e1d7b6ffcf81f73ee6ddd01f370cb836ede
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-08-10 09:51:03 +0300

    remove localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    host: true,  // هذا يفتح السيرفر على كل الواجهات، مش بس localhost
-    port: 5173,
-  },
-
-})
+  plugins: [react(), tailwindcss()],
+});
