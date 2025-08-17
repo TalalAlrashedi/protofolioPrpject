8fd59c2259d27082b9dcbd3accdd95f036bd8a60
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-08-17 22:26:40 +0300

    fix navbar for mobile % change position of dark mode and langugae btns & add text for theme decrease hover padding for navbar

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "192.168.8.254",
-    port: 5173,
-  },
 });
